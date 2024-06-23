17503cb6301d12864e295920db841084e60c4077
lighterEB <hellohus726@126.com>
2024-06-23 16:19:19 +0800

    调整卡片样式

--- frontend/src/components/Cards.vue
+++ frontend/src/components/Cards.vue
@@ -17,21 +17,21 @@ const props = defineProps({
 
 <style scoped>
 .card:hover{
-  box-shadow: rgb(0 0 0 / 30%) -20px 60px 30px -12px;
+  box-shadow: rgb(0 0 0 / 30%) -20px 20px 30px -12px;
   border-radius: 1.5rem;
-  border: 0.2rem solid lightblue;
+  border: 0.2rem solid #83c3cf;
   transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   width: 100%;
   height: 100%;
-  background-color: lightblue;
+  background-color: #83c3c6;
 }
 .card{
   box-shadow: rgb(0 0 0 / 30%) 0px 20px 30px -12px;
   border-radius: 1.5rem;
-  border: 0.2rem solid lightblue;
+  border: 0.2rem solid #83c3c6;
   transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   width: 100%;
   height: 100%;
-  background-color: lightblue;
+  background-color: #83c3c6;
 }
 </style>
