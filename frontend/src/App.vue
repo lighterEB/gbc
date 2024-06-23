f83f484117622f2c8b5e071017d5ba9a86fc7b4c
lighterEB <hellohus726@126.com>
2024-06-23 13:37:30 +0800

    删除文件

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,9 @@
-<template>
-  <div class="app">
-    <div class="page-container">
-      <button @click="toggleComponents">切换组件</button>
-      <component v-show="currentComponent"></component>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import { ref } from 'vue';
-import ComponentA from './components/ComponentA.vue';
-import ComponentB from './components/ComponentB.vue';
 
-let currentComponent = ref("ComponentA");
-
-const toggleComponents = () => {
-  currentComponent = (currentComponent === "ComponentA" ? "ComponentB" : "ComponentA");
-};
 </script>
+<template>
 
+</template>
 <style scoped>
-.page-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
+
 </style>
