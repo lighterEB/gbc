50159434de58b0c99087ed8fabe1f0e48dc8e7c9
lighterEB <hellohus726@126.com>
2024-06-23 23:48:26 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
-# README
+# 我要激活码
 
-## About
+## 关于
 
-This is the official Wails Vue template.
+这是一个由wails构建的本地应用程序，用于生成某IDE的激活码，如果你想用生成的激活码直接激活那是不可能的:(
 
-You can configure the project by editing `wails.json`. More information about the project settings can be found
-here: https://wails.io/docs/reference/project-config
+## 简介
 
-## Live Development
+使用 GO+VUE3+VITE+NAIVEUI
+主要是为了熟悉VUE生态写的这个程序，仅供学习交流。
 
-To run in live development mode, run `wails dev` in the project directory. This will run a Vite development
-server that will provide very fast hot reload of your frontend changes. If you want to develop in a browser
-and have access to your Go methods, there is also a dev server that runs on http://localhost:34115. Connect
-to this in your browser, and you can call your Go code from devtools.
-
-## Building
-
-To build a redistributable, production mode package, use `wails build`.
+暂时还没有完成。。后面再补
---
+++ backend/api/keygen.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// 获取证书
+	ca, _ := os.ReadFile("ca.crt")
+	certBase64 := base64.StdEncoding.EncodeToString(ca)
+
+	// 签名数据
+	licensePart := `{"licenseId":"7GC5726T07","licenseeName":"gurgles tumbles","assigneeName":"","assigneeEmail":"","licenseRestriction":"","checkConcurrentUse":false,"products":[{"code":"PCWMP","fallbackDate":"2026-09-14","paidUpTo":"2026-09-14","extended":true},{"code":"GO","fallbackDate":"2026-09-14","paidUpTo":"2026-09-14","extended":false},{"code":"PSI","fallbackDate":"2026-09-14","paidUpTo":"2026-09-14","extended":true}],"metadata":"0120230914PSAX000005","hash":"TRIAL:1805249793","gracePeriodDays":7,"autoProlongated":false,"isAutoProlongated":false}`
+	licensePartBytes := []byte(licensePart)
+	licensePartBase64 := base64.StdEncoding.EncodeToString(licensePartBytes)
+	licenseId := "7GC5726T07"
+	// 私钥
+	pk, err := parsePrivateKey()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 使用SHA1withRSA算法创建签名器
+	hashed := sha1.Sum(licensePartBytes)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, pk.(*rsa.PrivateKey), crypto.SHA1, hashed[:])
+	if err != nil {
+		fmt.Println("Failed to sign")
+	}
+	signBytes := []byte(signature)
+	signBase64 := base64.StdEncoding.EncodeToString(signBytes)
+
+	fmt.Printf("%s-%s-%s-%s", licenseId, licensePartBase64, signBase64, certBase64)
+
+}
+
+func parsePrivateKey() (interface{}, error) {
+	// 读取PEM文件
+	pemData, err := os.ReadFile("ca.key")
+	if err != nil {
+		return nil, err
+	}
+
+	// 解析PEM数据块
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block")
+	}
+
+	// 根据不同的加密算法进行特定的解析
+	var privateKey interface{}
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	case "EC PRIVATE KEY":
+		privateKey, err = x509.ParseECPrivateKey(block.Bytes)
+	default:
+		return privateKey, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
