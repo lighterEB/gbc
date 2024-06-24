9b9ee55c8d77a97e39a4285bccf8635d53cf9fd4
lighterEB <hellohus726@126.com>
2024-06-24 20:09:05 +0800

    后端增加响应文件

---
+++ backend/response/response.go
@@ -0,0 +1,44 @@
+package response
+
+type Response[T any] struct {
+	Code int
+	Msg  string
+	Data T
+}
+
+type Code struct {
+	Code int
+	Msg  string
+}
+
+const (
+	SuccessCode       = 200
+	NotFoundCode      = 404
+	InternalErrorCode = 500
+)
+
+func NewResponseCode(code int, msg string) *Code {
+	return &Code{Code: code, Msg: msg}
+}
+
+var (
+	Success  = NewResponseCode(SuccessCode, "Success")
+	NotFound = NewResponseCode(NotFoundCode, "Not Found")
+	Internal = NewResponseCode(InternalErrorCode, "Internal Error")
+)
+
+func NewSuccessResponse[T any](data T) *Response[T] {
+	return &Response[T]{
+		Code: SuccessCode,
+		Msg:  "Success",
+		Data: data,
+	}
+}
+
+func NewErrorResponse[T any](code int, msg string) *Response[T] {
+	return &Response[T]{
+		Code: code,
+		Msg:  msg,
+		Data: *new(T),
+	}
+}
